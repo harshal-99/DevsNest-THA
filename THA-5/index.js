630f3e68370a4c2deeded400ed384da0858b5402
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-06-13 15:34:19 -0700

    All functions implemented.

---
+++ THA-5/index.js
@@ -0,0 +1,82 @@
+function array_clone1(input) {
+    return input.filter(x => x)
+}
+
+function array_clone2(input) {
+    return Array.from(input, x => x)
+}
+
+function array_clone3(input) {
+    const new_array = []
+    input.forEach(element => {
+        new_array.push(element)
+    })
+    return new_array
+}
+
+function array_clone4(input) {
+    return input.map(x => x)
+}
+
+function array_clone5(input) {
+    return input.slice()
+}
+
+function array_clone6(input) {
+    return input.splice(0, input.length)
+}
+
+function is_array(input) {
+    return input instanceof Array
+}
+
+function first(array, n = 1) {
+    return array.splice(0, n)
+}
+
+function join_elements(array, separator = ',') {
+    return array.join(separator)
+}
+
+function max_frequency(array) {
+    const freq = new Map()
+    for (let i of array) {
+        if (!freq.has(i)) {
+            freq.set(i, 1)
+        } else {
+            freq.set(i, freq.get(i) + 1)
+        }
+    }
+
+    let iterator = freq.entries()
+    const result = iterator.next().value
+
+    freq.forEach((value, key) => {
+        if (result[1] < value) {
+            result[0] = key
+            result[1] = value
+        }
+    })
+
+    return `${result[0]} ( ${result[1]} times ) `
+}
+
+// console.log(array_clone5([1, 2, 3]))
+
+// console.log(array_clone2([1, 2, 4, 0]));
+// console.log(array_clone2([1, 2, [4, 0]]));
+
+// console.log(is_array('w3resource'))
+// console.log(is_array([1, 2, 4, 0]))
+
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+
+
+// myColor = ["Red", "Green", "White", "Black"]
+// console.log(join_elements(myColor))
+
+console.log(max_frequency([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
