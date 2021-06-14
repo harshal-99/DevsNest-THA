8dea98979d019e09925a05e706c3a6fbf7f5d1e0
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-06-14 16:15:02 -0700

    Implemented all functions.

--- THA-5/index.js
+++ THA-5/index.js
@@ -1,12 +1,12 @@
-export function array_clone1(input) {
+function array_clone1(input) {
     return input.filter(x => x)
 }
 
-export function array_clone2(input) {
+function array_clone2(input) {
     return Array.from(input, x => x)
 }
 
-export function array_clone3(input) {
+function array_clone3(input) {
     const new_array = []
     input.forEach(element => {
         new_array.push(element)
@@ -14,31 +14,34 @@ export function array_clone3(input) {
     return new_array
 }
 
-export function array_clone4(input) {
+function array_clone4(input) {
     return input.map(x => x)
 }
 
-export function array_clone5(input) {
+function array_clone5(input) {
     return input.slice()
 }
 
-export function array_clone6(input) {
+function array_clone6(input) {
     return input.splice(0, input.length)
 }
 
-export function is_array(input) {
+function is_array(input) {
     return input instanceof Array
 }
 
-export function first(array, n = 1) {
-    return array.splice(0, n)
+function first(array, n = 1) {
+    if (n <= 0) {
+        return []
+    }
+    return array.slice(0, n)
 }
 
-export function join_elements(array, separator = ',') {
+function join_elements(array, separator = ',') {
     return array.join(separator)
 }
 
-export function max_frequency(array) {
+function max_frequency(array) {
     const freq = new Map()
     for (let i of array) {
         if (!freq.has(i)) {
@@ -61,22 +64,23 @@ export function max_frequency(array) {
     return `${result[0]} ( ${result[1]} times )`
 }
 
-// console.log(array_clone5([1, 2, 3]))
+module.exports = {
+    array_clone1: array_clone1,
+    array_clone2: array_clone2,
+    array_clone3: array_clone3,
+    array_clone4: array_clone4,
+    array_clone5: array_clone5,
+    array_clone6: array_clone6,
+    is_array: is_array,
+    first: first,
+    join_elements: join_elements,
+    max_frequency: max_frequency,
+}
 
-// console.log(array_clone2([1, 2, 4, 0]));
-// console.log(array_clone2([1, 2, [4, 0]]));
+/*
+const array = [7, 9, 0, -2]
+let expected  = first(array, 3)
 
-// console.log(is_array('w3resource'))
-// console.log(is_array([1, 2, 4, 0]))
-
-// console.log(first([7, 9, 0, -2]));
-// console.log(first([],3));
-// console.log(first([7, 9, 0, -2],3));
-// console.log(first([7, 9, 0, -2],6));
-// console.log(first([7, 9, 0, -2],-3));
-
-
-// myColor = ["Red", "Green", "White", "Black"]
-// console.log(join_elements(myColor))
-
-// console.log(max_frequency([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
+for (let i = 0; i < 3; i++) {
+    console.log(array[i], expected[i])
+}*/
