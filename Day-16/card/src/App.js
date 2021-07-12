9d46de1a955ddd7462d56d1b0835d5e56c7da5ec
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-07-12 22:33:25 +0530

    Initial commit.
    Signed-off-by: Harshal Pawshekar <harshalpawshekar999@gmail.com>

---
+++ Day-16/card/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from './react-meme1.png'
+function App() {
+	return (
+		<div className="App">
+			<img src={image} alt=""/>
+			<h2>React Card</h2>
+			<p>Your main THA is to make meme cards</p>
+		</div>
+	)
+}
+
+export default App;
